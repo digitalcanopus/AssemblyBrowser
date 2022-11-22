7d8c4c5fd9e67ea8d23374f7739fbb3a92d0ae6e
digitalcanopus <rocky-cat@mail.ru>
2022-11-22 22:21:33 +0300

    Model added

--- lab3/Model.cs
+++ lab3/Model.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Library;
 
 namespace lab3
 {
     public static class Model
     {
-
+        public static InfAssembly GetTree(string filename)
+        {
+            GetAssembly getAssembly = new GetAssembly(filename);
+            return getAssembly.GetAssemblyInfo();
+        }
     }
 }
