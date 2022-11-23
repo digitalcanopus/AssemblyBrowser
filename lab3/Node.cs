3c000d5edd267c6b229b3558e4e7739e158208c4
digitalcanopus <rocky-cat@mail.ru>
2022-11-23 10:06:56 +0300

    VM added

---
+++ lab3/Node.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lab3
+{
+    public class Node
+    {
+        public string Name { get; set; }
+        public List<Node> Nodes { get; set; } = new List<Node>();
+    }
+}
---
+++ lab3/ViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace lab3
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        
+    }
+}
