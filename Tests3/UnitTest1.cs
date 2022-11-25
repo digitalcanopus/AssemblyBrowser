444ebd7a8c4f70eda7b0dbec6ed04332586eaddf
digitalcanopus <rocky-cat@mail.ru>
2022-11-25 12:16:27 +0300

    new tests added

--- Tests3/UnitTest1.cs
+++ Tests3/UnitTest1.cs
@@ -1,4 +1,5 @@
 using Library;
+using System;
 
 namespace Tests3
 {
@@ -57,6 +58,52 @@ namespace Tests3
             string type2 = assemblyInfo.Namespaces[0].Types[1].Type;
             Assert.AreEqual("Class", type1);
             Assert.AreEqual("Class", type2);
+        }
+
+        [TestMethod]
+        public void TFieldsQuantity()
+        {
+            assemblyInfo = getAssembly.GetAssemblyInfo();
+            int fields = assemblyInfo.Namespaces[0].Types[0].Fields.Count;
+            Assert.AreEqual(2, fields);
+        }
+
+        [TestMethod]
+        public void TFieldsName()
+        {
+            assemblyInfo = getAssembly.GetAssemblyInfo();
+            string field1 = assemblyInfo.Namespaces[0].Types[0].Fields[0].FieldName;
+            string field2 = assemblyInfo.Namespaces[0].Types[0].Fields[1].FieldName;
+            Assert.AreEqual("ThrInfList", field1);
+            Assert.AreEqual("tm", field2);
+        }
+
+        [TestMethod]
+        public void TMethodsQuantity()
+        {
+            assemblyInfo = getAssembly.GetAssemblyInfo();
+            int methods = assemblyInfo.Namespaces[0].Types[1].Methods.Count;
+            Assert.AreEqual(11, methods);
+        }
+
+        [TestMethod]
+        public void TMethodsType()
+        {
+            assemblyInfo = getAssembly.GetAssemblyInfo();
+            string method1 = assemblyInfo.Namespaces[0].Types[1].Methods[0].ReturnType;
+            string method2 = assemblyInfo.Namespaces[0].Types[1].Methods[4].ReturnType;
+            Assert.AreEqual("Void", method1);
+            Assert.AreEqual("MInfo", method2);
+        }
+
+        [TestMethod]
+        public void TMethodsParameters()
+        {
+            assemblyInfo = getAssembly.GetAssemblyInfo();
+            string parameter1 = assemblyInfo.Namespaces[0].Types[0].Methods[4].Parameters[0].FieldName;
+            string parameter2 = assemblyInfo.Namespaces[0].Types[0].Methods[4].Parameters[1].FieldName;
+            Assert.AreEqual("fName", parameter1);
+            Assert.AreEqual("sym", parameter2);
         }
     }
 }
